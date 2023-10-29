fcf3fbfcec852f1e6a14553a904b8d4e783e12f6
Hayasaka <146614468+kuromame18@users.noreply.github.com>
2023-10-29 04:09:53 +0000

    フォローボタン　フォロー・フォロワー数　追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,18 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
