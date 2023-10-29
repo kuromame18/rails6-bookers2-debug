8e7b979a5393dff7b92ec53281fa668485c64da7
Hayasaka <146614468+kuromame18@users.noreply.github.com>
2023-10-29 06:32:54 +0000

    フォロー・フォロワー機能　完了

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
-  end 
+  end
   resources :users, only: [:index,:show,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
