ce00819653c2c2e92fde67cfb5aba469d0e6d0a6
Hayasaka <146614468+kuromame18@users.noreply.github.com>
2023-11-04 16:35:52 +0000

    チャット機能コード記入　動作不良

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,9 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
+
+  get 'messages/:id' => 'messages#message', as: 'message'
+  post 'messages' => 'messages#create', as: 'messages'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
