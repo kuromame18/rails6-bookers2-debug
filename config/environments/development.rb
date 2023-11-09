a12f92a56300c379b48328a3a4bf5009386dfc2d
Hayasaka <146614468+kuromame18@users.noreply.github.com>
2023-11-09 06:55:02 +0000

    メール機能　実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,14 @@ Rails.application.configure do
   config.hosts << "531f49799fcb4550af2e1f2602cc5288.vfs.cloud9.ap-northeast-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+   address:              'smtp.gmail.com',
+   port:                  587,
+   domain:               'gmail.com',
+   user_name:            '送信元となるGmailアドレスを記入',
+   password:             'アプリパスワードを記入',
+   authentication:       'login',
+   enable_starttls_auto:  true
+  }
 end
